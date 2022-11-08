851a3e245bf3506a124cbae867929af515054e8d
e1b20027 <e1b20027@oit.ac.jp>
2022-11-08 15:56:22 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/pachimon/kaizi/pachimon/PachimonApplication.java
@@ -0,0 +1,13 @@
+package oit.is.pachimon.kaizi.pachimon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachimonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PachimonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/pachimon/kaizi/pachimon/PachimonApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.pachimon.kaizi.pachimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PachimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
