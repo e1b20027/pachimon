d1edcac040d2c06adfaefb6843866323af310f7f
Y-Saruwatari <e1b20049@st.oit.ac.jp>
2022-12-06 14:46:03 +0900

    結果表の作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
 INSERT INTO ptype (typeName) VALUES ('ほのお');
 INSERT INTO ptype (typeName) VALUES ('みず');
 INSERT INTO ptype (typeName) VALUES ('くさ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('ほのお','みず','まけ','かち');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('ほのお','くさ','かち','まけ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('ほのお','ほのお','あいこ','あいこ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('みず','みず','あいこ','あいこ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('みず','くさ','まけ','かち');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('みず','ほのお','かち','まけ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('くさ','みず','かち','まけ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('くさ','くさ','あいこ','あいこ');
+INSERT INTO result (p1type,p2type,p1result,p2result) VALUES ('くさ','ほのお','まけ','かち');
