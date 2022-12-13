09ba53e5f24bf1aba772b84417caa630a0fd8678
Y-Saruwatari <e1b20049@st.oit.ac.jp>
2022-12-13 16:49:56 +0900

    結果の表示(判定あり・後出しなし)

--- src/main/java/oit/is/pachimon/kaizi/pachimon/model/ResultMapper.java
+++ src/main/java/oit/is/pachimon/kaizi/pachimon/model/ResultMapper.java
@@ -10,5 +10,8 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface ResultMapper {
   @Select("SELECT p1result from result where p1type = #{p1type} and p2type = #{p2type}")
-  String selectById(String p1type, String p2type);
+  String selectp1(String p1type, String p2type);
+
+  @Select("SELECT p2result from result where p1type = #{p1type} and p2type = #{p2type}")
+  String selectp2(String p1type, String p2type);
 }
