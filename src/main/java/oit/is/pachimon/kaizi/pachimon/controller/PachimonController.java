1c0b8b154fde5ac99eb9626563e33b33192abe97
e1b20027 <e1b20027@oit.ac.jp>
2023-01-17 13:09:41 +0900

    ゲーム

--- src/main/java/oit/is/pachimon/kaizi/pachimon/controller/PachimonController.java
+++ src/main/java/oit/is/pachimon/kaizi/pachimon/controller/PachimonController.java
@@ -36,6 +36,7 @@ public class PachimonController {
     this.room.resetHand();
     this.room.resetResult();
     this.room.resetlogout();
+    this.room.resetscore();
     return "home.html";
   }
 
@@ -66,7 +67,11 @@ public class PachimonController {
     this.room.resetm1();
     this.room.resetm2();
     this.room.resetk();
+    this.room.resetscore();
+    this.room.resetflag();
     int cnt = this.room.checkGetUser(loginUser);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("handm1", this.room.getm1(cnt));
     model.addAttribute("handm2", this.room.getm2(cnt));
@@ -87,33 +92,10 @@ public class PachimonController {
     this.room.resetCount();
     this.room.resetHand();
     this.room.resetResult();
+    this.room.resetflag();
     int cnt = this.room.checkGetUser(loginUser);
-    model.addAttribute("handm", this.room.getm(cnt));
-    model.addAttribute("handm1", this.room.getm1(cnt));
-    model.addAttribute("handm2", this.room.getm2(cnt));
-    model.addAttribute("handk", this.room.getk(cnt));
-    model.addAttribute("handh", this.room.geth(cnt));
-    model.addAttribute("logoutflag", this.room.getlogout());
-    model.addAttribute("hand", this.room.getHand(cnt));
-    model.addAttribute("login_user", loginUser);
-    model.addAttribute("login_users", room.getUsers());
-    model.addAttribute("count", this.room.getCount());
-    model.addAttribute("result_count", this.room.getResult());
-    return "battle.html";
-  }
-
-  @GetMapping("restart2")
-  public String restart2(Principal prin, ModelMap model) {
-    String loginUser = prin.getName();
-    this.room.resetCount();
-    this.room.resetHand();
-    this.room.resetResult();
-    this.room.reseth();
-    this.room.resetm();
-    this.room.resetm1();
-    this.room.resetm2();
-    this.room.resetk();
-    int cnt = this.room.checkGetUser(loginUser);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("handm1", this.room.getm1(cnt));
     model.addAttribute("handm2", this.room.getm2(cnt));
@@ -141,7 +123,11 @@ public class PachimonController {
     this.room.resetm();
     this.room.resetm1();
     this.room.resetm2();
+    this.room.resetscore();
+    this.room.resetflag();
     int cnt = this.room.checkGetUser(loginUser);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("handm1", this.room.getm1(cnt));
     model.addAttribute("handm2", this.room.getm2(cnt));
@@ -165,7 +151,10 @@ public class PachimonController {
     this.room.resetCount();
     this.room.resetHand();
     this.room.resetResult();
+    this.room.resetflag();
     int cnt = this.room.checkGetUser(loginUser);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("handm1", this.room.getm1(cnt));
     model.addAttribute("handm2", this.room.getm2(cnt));
@@ -187,6 +176,8 @@ public class PachimonController {
     int cnt = this.room.checkGetUser(loginUser);
     this.room.resetResult();
     this.room.seth(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handh", this.room.geth(cnt));
     model.addAttribute("logoutflag", this.room.getlogout());
     model.addAttribute("login_user", loginUser);
@@ -204,6 +195,8 @@ public class PachimonController {
     int cnt = this.room.checkGetUser(loginUser);
     this.room.resetResult();
     this.room.setm(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("logoutflag", this.room.getlogout());
     model.addAttribute("login_user", loginUser);
@@ -221,6 +214,8 @@ public class PachimonController {
     int cnt = this.room.checkGetUser(loginUser);
     this.room.resetResult();
     this.room.setm1(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm1", this.room.getm1(cnt));
     model.addAttribute("logoutflag", this.room.getlogout());
     model.addAttribute("login_user", loginUser);
@@ -238,6 +233,8 @@ public class PachimonController {
     int cnt = this.room.checkGetUser(loginUser);
     this.room.resetResult();
     this.room.setm2(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handm2", this.room.getm2(cnt));
     model.addAttribute("logoutflag", this.room.getlogout());
     model.addAttribute("login_user", loginUser);
@@ -255,6 +252,8 @@ public class PachimonController {
     int cnt = this.room.checkGetUser(loginUser);
     this.room.resetResult();
     this.room.setk(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     model.addAttribute("handk", this.room.getk(cnt));
     model.addAttribute("logoutflag", this.room.getlogout());
     model.addAttribute("login_user", loginUser);
@@ -270,6 +269,8 @@ public class PachimonController {
   public String result(Principal prin, ModelMap model) {
     String loginUser = prin.getName();
     int cnt = this.room.checkGetUser(loginUser);//添え字
+    int flag1 = this.room.getflag(0);
+    int flag2 = this.room.getflag(1);
     this.room.setResult(cnt);
     model.addAttribute("handm", this.room.getm(cnt));
     model.addAttribute("handm1", this.room.getm1(cnt));
@@ -282,17 +283,56 @@ public class PachimonController {
     model.addAttribute("count", this.room.getCount());
     model.addAttribute("result_count", this.room.getResult());
     model.addAttribute("logoutflag", this.room.getlogout());
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     if (cnt == 0) {
       String result2 = resultMapper.selectp1(this.room.getHand(0), this.room.getHand(1));
       model.addAttribute("result2", result2);
       model.addAttribute("player1_hand", this.room.getHand(0));
       model.addAttribute("player2_hand", this.room.getHand(1));
+      if (flag1 == 0) {
+        switch (result2) {
+          case "かち":
+            if (this.room.getHand(0) == "ほのお" && this.room.getHand(1) == "くさ") {
+              this.room.setscore(cnt, 10);
+            } else {
+              this.room.setscore(cnt, 3);
+            }
+            break;
+          case "あいこ":
+            this.room.setscore(cnt, 2);
+            break;
+          case "まけ":
+            this.room.setscore(cnt, 1);
+            break;
+        }
+      }
     } else if (cnt == 1) {
       String result2 = resultMapper.selectp2(this.room.getHand(0), this.room.getHand(1));
       model.addAttribute("result2", result2);
       model.addAttribute("player1_hand", this.room.getHand(1));
       model.addAttribute("player2_hand", this.room.getHand(0));
+      if (flag2 == 0) {
+        switch (result2) {
+          case "かち":
+            if (this.room.getHand(1) == "ほのお" && this.room.getHand(0) == "くさ") {
+              this.room.setscore(cnt, 10);
+            } else {
+              this.room.setscore(cnt, 3);
+            }
+            break;
+          case "あいこ":
+            this.room.setscore(cnt, 2);
+            break;
+          case "まけ":
+            this.room.setscore(cnt, 1);
+            break;
+        }
+      }
     }
+    this.room.setflag(cnt);
+    model.addAttribute("player1_score", this.room.getscore(0));
+    model.addAttribute("player2_score", this.room.getscore(1));
     return "battle.html";
   }
 
